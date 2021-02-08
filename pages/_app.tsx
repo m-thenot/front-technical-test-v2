f289f7744f9c7530585b13f3902a3dadb037ed37
Martin <martinthenot@gmail.com>
2021-02-08 11:41:23 +0100

    implement material ui css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "instantsearch.css/themes/algolia.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
