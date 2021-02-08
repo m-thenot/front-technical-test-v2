96a3c753c010aea1b5fd179d34757f8c694f09c6
Martin <martinthenot@gmail.com>
2021-02-08 13:07:49 +0100

    connect redux store to the application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "instantsearch.css/themes/algolia.css";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Fragment } from "react";
+import { wrapper } from "../src/store";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,4 +13,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/static/favicon.ico" />
         </Head>
 
         <body>
